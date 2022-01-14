3c1fe0ea398ef091d38d8a1e7cb5fddfd73e1546
Anton Sudakov <sudakov.ant@gmail.com>
2022-01-14 13:31:25 +0300

    Initial Commit

---
+++ QiuzTime!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QiuzTime!
+//
+//  Created by Антон Судаков on 14.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
